ac6cd9a1cc705886f0ed32394f30e61d0127e7e2
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-11-02 11:34:11 +0200

    nice

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import ToDo from "./ToDo";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [todo, setTodo] = useState({});
+  const [loading, setLoading] = useState(true);
 
-  const [todo, setTodo] = useState({
-    userId: 1,
-    id: 1,
-    title: "delectus aut autem",
-    completed: true,
-  });
+  useEffect(() => {
+    console.log("Bonjour");
+    async function getData() {
+      const response = await fetch("https://jsonplaceholder.typicode.com/todos/4");
+      const data = await response.json();
+    console.log(data);
+    setTodo(data);
+    setLoading(false);
+    }
+    getData();
+  }, []);
+  // const [todo, setTodo] = useState({
+  //   userId: 1,
+  //   id: 1,
+  //   title: "delectus aut autem",
+  //   completed: true,
+  // });
 
   return (
     <div className="App">
       <h1>bonjour</h1>
-      <ToDo 
-      userId={todo.userId}
-      id={todo.id}
-      title={todo.title}
-      completed={todo.completed}
-      />
+    {loading ? <p>Ladejas...</p> : <ToDo {...todo} />}
     </div>
 
     
